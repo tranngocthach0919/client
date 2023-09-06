bb6e9e3ed6c2d7cba16f4706b4c1ac76f5ebd5c2
tranngocthach <tranngocthach0919@gmail.com>
2023-09-06 14:11:32 +0700

    first commit

---
+++ src/pages/product-detail/product-detail.component.js
@@ -0,0 +1,12 @@
+
+
+const ProductDetail = () => {
+    return (
+        <div className="flex justify-bewteen">
+            <div className="w-3/5 bg-gray-200">img</div>
+            <div className="w-2/5 bg-gray-600"></div>
+        </div>
+    );
+};
+
+export default ProductDetail;
